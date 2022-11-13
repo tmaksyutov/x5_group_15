5809bc03c4993fd765186690eaf186303c9ef5ee
TMaksyutov <dadojkeee@mail.ru>
2022-11-13 12:56:34 +0300

    edit TestBase

---
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -0,0 +1,38 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
+import com.demoqa.tests.properties.SystemProperties;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+
+    @BeforeAll
+    static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.browser = SystemProperties.browser();
+        Configuration.browserVersion = SystemProperties.browserVersion();
+        Configuration.browserSize = SystemProperties.browserSize();
+        if (System.getProperty("remoteUrl") != null) {
+            Configuration.remote = System.getProperty("remoteUrl");
+        }
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
+
