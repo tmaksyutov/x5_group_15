94a9aab358e6b624051ba7086525b4f6c2f20a0f
TMaksyutov <dadojkeee@mail.ru>
2022-11-13 12:55:09 +0300

    added utils

---
+++ src/test/java/com/demoqa/utils/DriverUtils.java
@@ -0,0 +1,11 @@
+package com.demoqa.utils;
+
+import com.codeborne.selenide.Selenide;
+
+import static org.openqa.selenium.logging.LogType.BROWSER;
+
+public class DriverUtils {
+    public static String getConsoleLogs() { // todo refactor
+        return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
+    }
+}
