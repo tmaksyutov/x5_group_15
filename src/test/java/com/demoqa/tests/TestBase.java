28608baf4aadc619e857f3f73e093bd20eee44b0
TMaksyutov <dadojkeee@mail.ru>
2022-11-13 13:48:15 +0300

    edit settings

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -8,29 +8,23 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.URI;
-
-import static java.lang.String.format;
 
 public class TestBase {
 
     @BeforeAll
     static void configure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
         DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("browserName", System.getProperty("browserName", "chrome"));
-        capabilities.setCapability("browserVersion", System.getProperty("browserVersion", "99"));
-        Configuration.browserCapabilities = capabilities;
-        Configuration.browserSize = System.getProperty("browserSize", "2560x1440");
-
-        if (System.getProperty("remote") != null)
-            Configuration.remote = format("https://%s:%s@%s/wd/hub", System.getProperty("user"), System.getProperty("password"), System.getProperty("remote"));
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.browser = SystemProperties.browser();
+        Configuration.browserVersion = SystemProperties.browserVersion();
+        Configuration.browserSize = SystemProperties.browserSize();
+        if (System.getProperty("remoteUrl") != null) {
+            Configuration.remote = System.getProperty("remoteUrl");
+        }
     }
 
     @AfterEach
